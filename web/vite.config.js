50b4e0c0b6e74c8537dfb4ae7dc0d60821be267a
Jing Wang <yuzhounh@163.com>
2026-09-26 19:03:49 +0800

    feat(web): enable devOptions for PWA testing in local dev server

--- web/vite.config.js
+++ web/vite.config.js
@@ -31,6 +31,9 @@ export default defineConfig({
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2,ttf}'],
       },
+      devOptions: {
+        enabled: true,
+      },
     }),
   ],
   server: {
